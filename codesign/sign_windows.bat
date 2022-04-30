e1cd477a0a5355f76e44a24ebdc253e981eb4189
Maciej Szumocki <maciej.szumocki@reef.pl>
2022-04-30 13:39:11 +0200

    Code signing for Windows executable

---
+++ codesign/sign_windows.bat
@@ -0,0 +1,1 @@
+"C:/Program Files (x86)/Windows Kits/10/bin/10.0.17763.0/x86/signtool.exe" sign /v /debug /f certificate_encrypted.pfx /fd SHA256 /p %CODESIGN_PASSWORD% /tr http://timestamp.sectigo.com /td SHA256 ..\dist\byteanalytics-encoder.exe"
